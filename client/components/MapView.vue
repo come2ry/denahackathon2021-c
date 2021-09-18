bd861d24bd0c2c4b80b889e8f8a5cce4ddae5aea
bababax11 <38175513+bababax11@users.noreply.github.com>
2021-09-19 00:15:09 +0900

    fix

--- client/components/MapView.vue
+++ client/components/MapView.vue
@@ -14,17 +14,28 @@
 <script lang="ts">
 import Vue from 'vue'
 
-type LL = [number, number]
+type LL = {
+  lat: number
+  lng: number
+}
 export default Vue.extend({
   data() {
-    const logs: LL[] = []
+    const logs: LL[] = [
+      [35.658318, 139.702231],
+      [35.6583, 139.702232],
+      [35.6582, 139.702232],
+      [35.6581, 139.702235]
+    ].map((e) => ({
+      lat: e[0],
+      lng: e[1]
+    }))
     return {
-      lat: null,
-      lng: null,
+      lat: 35.658319,
+      lng: 139.702232,
       logs
     } as {
-      lat: number | null
-      lng: number | null
+      lat: number
+      lng: number
       logs: LL[]
     }
   },
@@ -44,17 +55,26 @@ export default Vue.extend({
     }
   },
   mounted() {
-    console.log(this.$L)
-
     // GPS センサの値が変化したら何らか実行する geolocation.watchPosition メソッド
     navigator.geolocation.watchPosition(
       (position) => {
+        const myMap = this.$L.map('mapid')
+        console.log(myMap)
+        this.$L
+          .polyline(this.logs as LL[], {
+            color: 'green',
+            weight: 2,
+            fill: true,
+            fillColor: 'green',
+            opacity: 0.5
+          })
+          .addTo(myMap)
         const lat = position.coords.latitude // 緯度を取得
         const lng = position.coords.longitude // 経度を取得
         // const accu = position.coords.accuracy // 緯度・経度の精度を取得
         this.lat = lat
         this.lng = lng
-        this.logs.push([lat, lng])
+        this.logs.push({ lat, lng })
       },
       (error) => {
         console.log(error)
