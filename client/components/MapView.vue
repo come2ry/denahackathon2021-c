4998c024a0deae4becc180a8d0973ba49d405138
yud0uhu <cist.b219.y.osaka@gmail.com>
2021-09-18 17:30:16 +0900

    add module

---
+++ client/components/MapView.vue
@@ -0,0 +1,28 @@
+<template>
+  <l-map style="height: 300px" :zoom="zoom" :center="center">
+    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+    <l-marker :lat-lng="markerLatLng"></l-marker>
+  </l-map>
+</template>
+
+<script>
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+  },
+  data() {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 15,
+      center: [51.505, -0.159],
+      markerLatLng: [51.504, -0.159],
+    }
+  },
+}
+</script>
