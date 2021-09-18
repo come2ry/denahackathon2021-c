c78d4bc1e1a3769568d55b2d5739700df56fd895
bababax11 <38175513+bababax11@users.noreply.github.com>
2021-09-18 18:25:38 +0900

    fix: nuxt-leaflet

--- client/components/MapView.vue
+++ client/components/MapView.vue
@@ -1,18 +1,20 @@
 <template>
-  <l-map style="height: 300px" :zoom="zoom" :center="center">
-    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-    <l-marker :lat-lng="markerLatLng"></l-marker>
-  </l-map>
+  <no-ssr>
+    <l-map style="height: 300px" :zoom="zoom" :center="center">
+      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      <l-marker :lat-lng="markerLatLng"></l-marker>
+    </l-map>
+  </no-ssr>
 </template>
 
 <script>
-import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
+import { LMap, LTileLayer, LMarker } from 'nuxt-leaflet'
 
 export default {
   components: {
     LMap,
     LTileLayer,
-    LMarker,
+    LMarker
   },
   data() {
     return {
@@ -21,8 +23,8 @@ export default {
         '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       zoom: 15,
       center: [51.505, -0.159],
-      markerLatLng: [51.504, -0.159],
+      markerLatLng: [51.504, -0.159]
     }
-  },
+  }
 }
 </script>
