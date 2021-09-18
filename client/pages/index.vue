594b22778eeb887c3d1b1bc58a2f761be1c9ca7f
yud0uhu <cist.b219.y.osaka@gmail.com>
2021-09-18 17:36:04 +0900

    Revert "add module"
    This reverts commit 4998c024a0deae4becc180a8d0973ba49d405138.

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -2,58 +2,49 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <v-card class="logo py-4 d-flex justify-center">
-        <MapView />
+        <NuxtLogo />
+        <VuetifyLogo />
       </v-card>
       <v-card>
         <v-card-title class="headline">
           Welcome to the Vuetify + Nuxt.js template
         </v-card-title>
         <v-card-text>
+          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
           <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
+            For more information on Vuetify, check out the <a
               href="https://vuetifyjs.com"
               target="_blank"
               rel="noopener noreferrer"
             >
-              documentation </a
-            >.
+              documentation
+            </a>.
           </p>
           <p>
-            If you have questions, please join the official
-            <a
+            If you have questions, please join the official <a
               href="https://chat.vuetifyjs.com/"
               target="_blank"
               rel="noopener noreferrer"
               title="chat"
             >
-              discord </a
-            >.
+              discord
+            </a>.
           </p>
           <p>
-            Find a bug? Report it on the github
-            <a
+            Find a bug? Report it on the github <a
               href="https://github.com/vuetifyjs/vuetify/issues"
               target="_blank"
               rel="noopener noreferrer"
               title="contribute"
             >
-              issue board </a
-            >.
+              issue board
+            </a>.
           </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
+          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
           <div class="text-xs-right">
             <em><small>&mdash; John Leider</small></em>
           </div>
-          <hr class="my-3" />
+          <hr class="my-3">
           <a
             href="https://nuxtjs.org/"
             target="_blank"
@@ -61,7 +52,7 @@
           >
             Nuxt Documentation
           </a>
-          <br />
+          <br>
           <a
             href="https://github.com/nuxt/nuxt.js"
             target="_blank"
@@ -72,7 +63,13 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
+          <v-btn
+            color="primary"
+            nuxt
+            to="/inspire"
+          >
+            Continue
+          </v-btn>
         </v-card-actions>
       </v-card>
     </v-col>
