d6f2fce0dc74bd4bb675ab3f0c931ba704657dfe
bababax11 <38175513+bababax11@users.noreply.github.com>
2021-09-19 09:53:57 +0900

    update types of log function

--- client/utils/log.ts
+++ client/utils/log.ts
@@ -1,6 +1,5 @@
 import dayjs from 'dayjs'
 import { LL, DLL } from './baseType'
-import { Err, Ok, Result } from './result'
 
 export function updateLog(logs: DLL[]): DLL[] {
   return logs.filter((e) => e.date.isAfter(dayjs().subtract(15, 'minutes')))
@@ -11,8 +10,8 @@ export function findLoci(logs: LL[]): LL[][] {
   const loops = []
   while (true) {
     const locus = _findLocus(_logs)
-    if (locus.isOk()) {
-      const [loop, rest] = locus.v
+    if (locus != null) {
+      const [loop, rest] = locus
       loops.push(loop)
       _logs = rest
     } else {
@@ -22,20 +21,20 @@ export function findLoci(logs: LL[]): LL[][] {
   return loops
 }
 
-function _findLocus(logs: LL[]): Result<[LL[], LL[]], null> {
+function _findLocus(logs: LL[]): [LL[], LL[]] | null {
   for (let end = 2; end < logs.length - 1; end++) {
     // 隣り合う線分は除く
     for (let j = 0; j < end - 1; j++) {
       if (checkCross(end, j, logs)) {
         // end + 1 までで線分
-        return new Ok([
+        return [
           logs.slice(j, end + 2),
           logs.slice(0, j + 1).concat(logs.slice(end + 1))
-        ])
+        ]
       }
     }
   }
-  return new Err(null)
+  return null
 }
 
 export function checkCross(i: number, j: number, logs: LL[]): boolean {
