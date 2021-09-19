02f4022ff3cb93da3b7dd346030dae4ded9550b6
come2ry <raiki5100@gmail.com>
2021-09-19 13:42:55 +0900

    TMP

--- server/database.py
+++ server/database.py
@@ -4,7 +4,7 @@ from core.config import settings
 from sqlalchemy import create_engine  # type: ignore
 from sqlalchemy.ext.declarative import declarative_base  # type: ignore
 from sqlalchemy.orm import scoped_session, sessionmaker  # type: ignore
-from config
+import config
 
 engine = create_engine(
     config.DevelopmentConfig.SQLALCHEMY_DATABASE_URI,
